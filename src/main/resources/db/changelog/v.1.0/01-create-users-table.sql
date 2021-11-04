1d57fc3f3f1ef55634c9172af50dac2227ef19a1
Sergey-GG <guesttvas@gmail.com>
2021-11-04 14:32:56 +0200

    Improving changelogs and changeSets

--- src/main/resources/db/changelog/v.1.0/01-create-users-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE table users(
-    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(25) not null,
-    surname varchar(30) not null,
-    email varchar(65) not null,
-    role varchar(20) not null
-)
