6fa4b78d6870f2408f4fc41b2fc3c7020d2065b9
Sergey-GG <guesttvas@gmail.com>
2021-12-06 01:36:46 +0200

    Updating test class

--- src/test/java/springboot/webapp/usersmanager/controllers_tests/UserControllerTests.java
+++ src/test/java/springboot/webapp/usersmanager/controllers_tests/UserControllerTests.java
@@ -1,17 +1,14 @@
 package springboot.webapp.usersmanager.controllers_tests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.ResponseEntity;
 import springboot.webapp.usersmanager.controllers.UserController;
 import springboot.webapp.usersmanager.entities.Role;
 import springboot.webapp.usersmanager.entities.User;
@@ -26,17 +23,23 @@ import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(UserController.class)
+@ExtendWith(MockitoExtension.class)
 public class UserControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+//    @Autowired
+//    private MockMvc mockMvc;
+////
+//    @Autowired
+//    private ObjectMapper mapper;
+//
+//    @MockBean
+//    private UserService userService;
 
-    @Autowired
-    private ObjectMapper mapper;
+    @InjectMocks
+    UserController userController;
 
-    @MockBean
-    private UserService userService;
+    @Mock
+    UserService userService;
 
 
     @Test
@@ -46,16 +49,18 @@ public class UserControllerTests {
 
         when(userService.getAll()).thenReturn(users);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/users")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.OK.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(not(emptyString())));
-        MatcherAssert.assertThat(response.getContentAsString(), is(not("[]")));
+        ResponseEntity<List<User>> response = userController.getAll();
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.OK.value()));
+        MatcherAssert.assertThat(response.getBody(), is(notNullValue()));
+        MatcherAssert.assertThat(response.getBody(), is(not(empty())));
+        MatcherAssert.assertThat(response.getBody().toString(), is(not("[]")));
     }
 
     @Test
@@ -65,15 +70,17 @@ public class UserControllerTests {
 
         when(userService.getAll()).thenReturn(users);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/users")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.OK.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is("[]"));
+        ResponseEntity<List<User>> response = userController.getAll();
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.OK.value()));
+        MatcherAssert.assertThat(response.getBody(), is(notNullValue()));
+        MatcherAssert.assertThat(response.getBody().toString(), is("[]"));
     }
 
 
@@ -84,15 +91,16 @@ public class UserControllerTests {
 
         when(userService.get(user.getId())).thenReturn(Optional.of(user));
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/" + user.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/users/" + user.getId())
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.OK.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(not(emptyString())));
+        ResponseEntity<User> response = userController.get(user.getId());
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.OK.value()));
+        MatcherAssert.assertThat(response.getBody(), is(notNullValue()));
     }
 
     @Test
@@ -100,15 +108,16 @@ public class UserControllerTests {
     public void getByIdWhenNonExistentUserStatusNotFound() {
         when(userService.get(1)).thenReturn(Optional.empty());
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/2")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/users/2")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.NOT_FOUND.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(emptyString()));
+        ResponseEntity<User> response = userController.get(1);
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.NOT_FOUND.value()));
+        MatcherAssert.assertThat(response.getBody(), is(nullValue()));
     }
 
     @Test
@@ -118,17 +127,19 @@ public class UserControllerTests {
 
         when(userService.put(user)).thenReturn(true);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .put("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .content(this.mapper.writeValueAsString(user)))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .put("/users")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .content(this.mapper.writeValueAsString(user)))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.OK.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is("true"));
+        ResponseEntity<Boolean> response = userController.put(user);
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.OK.value()));
+        MatcherAssert.assertThat(response.getBody(), is(notNullValue()));
+        MatcherAssert.assertThat(response.getBody().toString(), is("true"));
     }
 
     @Test
@@ -136,17 +147,19 @@ public class UserControllerTests {
     public void putWhenUserWithExistedEmailStatusConflict() {
         when(userService.put(any())).thenThrow(IllegalArgumentException.class);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .put("/users")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .content(this.mapper.writeValueAsString(getUser()))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .put("/users")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .content(this.mapper.writeValueAsString(getUser()))
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.CONFLICT.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is("false"));
+        ResponseEntity<Boolean> response = userController.put(any());
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.CONFLICT.value()));
+        MatcherAssert.assertThat(response.getBody(), is(notNullValue()));
+        MatcherAssert.assertThat(response.getBody().toString(), is("false"));
     }
 
     @Test
@@ -156,15 +169,16 @@ public class UserControllerTests {
 
         when(userService.delete(user.getId())).thenReturn(true);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/users/" + user.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .delete("/users/" + user.getId())
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(equalTo(HttpStatus.OK.value())));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(emptyString()));
+        ResponseEntity<Void> response = userController.delete(user.getId());
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(equalTo(HttpStatus.OK.value())));
+        MatcherAssert.assertThat(response.getBody(), is(nullValue()));
     }
 
     @Test
@@ -172,15 +186,16 @@ public class UserControllerTests {
     public void deleteByIdWhenNonExistentUserStatusNotFound() {
         when(userService.delete(7)).thenReturn(false);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/users/7")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+//        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+//                        .delete("/users/7")
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn()
+//                .getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), is(HttpStatus.NOT_FOUND.value()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(notNullValue()));
-        MatcherAssert.assertThat(response.getContentAsString(), is(emptyString()));
+        ResponseEntity<Void> response = userController.delete(7);
+
+        MatcherAssert.assertThat(response.getStatusCodeValue(), is(HttpStatus.NOT_FOUND.value()));
+        MatcherAssert.assertThat(response.getBody(), is(nullValue()));
     }
 
     public User getUser() {
