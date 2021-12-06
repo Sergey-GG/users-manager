9e52205ae2a50b14ea6dd081a123cb73a221b515
Sergey-GG <83033399+Sergey-GG@users.noreply.github.com>
2021-12-06 17:42:36 +0200

    Update UserGenerator.java

--- src/test/java/springboot/webapp/usersmanager/UserGenerator.java
+++ src/test/java/springboot/webapp/usersmanager/UserGenerator.java
@@ -9,12 +9,12 @@ import java.util.UUID;
 public class UserGenerator {
    public static User getUser() {
       Role [] values = Role.values();
-        return new User(
-                new Random().nextInt(100),
-                UUID.randomUUID().toString(),
-                UUID.randomUUID().toString(),
-                UUID.randomUUID().toString(),
-                values[new Random().nextInt(values.length)]
+      return new User(
+              new Random().nextInt(100),
+              UUID.randomUUID().toString(),
+              UUID.randomUUID().toString(),
+              UUID.randomUUID().toString(),
+              values[new Random().nextInt(values.length)]
         );
     }
 }
