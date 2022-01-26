9c16002a9f208bf870bdff5d484b570af92a85f4
Serhii <guesttvas@gmail.com>
2022-01-26 17:28:25 +0200

    Repository updating

--- src/main/java/springboot/webapp/usersmanager/repositories/PolygonRepositoryImpl.java
+++ src/main/java/springboot/webapp/usersmanager/repositories/PolygonRepositoryImpl.java
@@ -3,6 +3,7 @@ package springboot.webapp.usersmanager.repositories;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.jooq.*;
+import org.jooq.impl.SQLDataType;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.io.WKTReader;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import springboot.webapp.usersmanager.entities.Polygon;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
 
 import static springboot.webapp.usersmanager.generated_sources.jooq.Tables.POLYGON;
 
@@ -35,47 +37,49 @@ public class PolygonRepositoryImpl implements PolygonRepository {
     @Override
     @SneakyThrows
     public Polygon put(Polygon polygon) {
-            if (existsById(polygon.getId())) {
-                return Objects.requireNonNull(dslContext.update(POLYGON)
-                                .set(POLYGON.AREA, polygon.getArea())
-                                .set(POLYGON.GEOMETRY, new WKTReader(new GeometryFactory()).read(polygon.getGeometry()))
-                                .where(POLYGON.ID.eq(polygon.getId()))
-                                .returning()
-                                .fetchOne())
-                        .into(Polygon.class);
-            }
-            return Objects.requireNonNull(dslContext.insertInto(POLYGON,
-                                    POLYGON.ID,
-                                    POLYGON.AREA,
-                                    POLYGON.GEOMETRY
-                            )
-                            .values(polygon.getId(),
-                                    polygon.getArea(),
-                                    new WKTReader(new GeometryFactory()).read(polygon.getGeometry())
-                            )
+        if (existsById(polygon.getId())) {
+            return Objects.requireNonNull(dslContext.update(POLYGON)
+                            .set(POLYGON.AREA, polygon.getArea())
+                            .set(POLYGON.GEOMETRY, /*stGeomFromText()*/
+                                    new WKTReader(new GeometryFactory()).read(polygon.getGeometry()))
+                            .where(POLYGON.ID.eq(polygon.getId()))
                             .returning()
                             .fetchOne())
                     .into(Polygon.class);
+        }
+        return Objects.requireNonNull(dslContext.insertInto(POLYGON,
+                                POLYGON.ID,
+                                POLYGON.AREA,
+                                POLYGON.GEOMETRY
+                        )
+                        .values(polygon.getId(),
+                                polygon.getArea(),
+                                /*stGeomFromText()*/
+                                new WKTReader(new GeometryFactory()).read(polygon.getGeometry())
+                        )
+                        .returning()
+                        .fetchOne())
+                .into(Polygon.class);
     }
 
     @Override
     public List<springboot.webapp.usersmanager.entities.Polygon> findAll() {
-        return dslContext.select()
+        return dslContext.select(/*stAsText()*/)
                 .from(POLYGON)
                 .fetchInto(springboot.webapp.usersmanager.entities.Polygon.class);
     }
 
     @Override
     public Optional<springboot.webapp.usersmanager.entities.Polygon> findById(long id) {
-        return dslContext.selectFrom(POLYGON)
+
+        return dslContext.selectFrom(/*stAsText()*/ POLYGON)
                 .where(POLYGON.ID.eq(id))
                 .fetchOptionalInto(springboot.webapp.usersmanager.entities.Polygon.class);
     }
 
-
-//    public void example(Polygon polygon){
-//        dslContext
-//                .select(stGeomFromText(polygon.getGeometry))
-//                .fetch();
+//    public void example(org.locationtech.jts.geom.Polygon polygon, Binding binding){
+//        DataType<Polygon> polygonType = SQLDataType.OTHER.asConvertedDataType(binding);
+//        Field<Polygon> bindValue = dslContext.val(polygon, polygonType);
+//        dslContext.condition("ST_WITHIN({0}, {1})", POLYGON.GEOMETRY, bindValue);
 //    }
 }
